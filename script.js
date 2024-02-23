1341701439cd029578e7dbcdfa3451fed14995bf
Samajit Kundu <126185958+SamajitKundu123@users.noreply.github.com>
2024-02-23 21:42:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const paragraphs = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et consequat enim. Ut porta elementum nisi, at auctor nibh feugiat et. Donec placerat lectus quis scelerisque gravida. Pellentesque rhoncus erat mollis feugiat aliquam. Suspendisse velit ipsum, elementum in ultricies eget, venenatis ac est. Etiam ac vehicula ante, vitae vestibulum felis. Quisque id bibendum turpis. Pellentesque commodo euismod tellus, sit amet lobortis nisl auctor ac. Nunc sagittis a odio ut congue. Phasellus viverra tincidunt dui eu dignissim. Suspendisse eget est a lorem pulvinar pellentesque fermentum vel est. Donec elementum magna in massa mattis, sit amet dignissim libero elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque a neque venenatis, vestibulum lacus eget, rhoncus eros.',
+
+'Sed congue, nulla vitae accumsan convallis, massa lorem dapibus ipsum, sit amet pellentesque nulla neque in odio. Nullam felis velit, ultricies ac fringilla ac, fringilla eu mi. Ut at erat eget metus fringilla fringilla a sit amet enim. Donec fermentum metus a libero sagittis scelerisque. Phasellus et facilisis felis. Quisque vitae sapien mollis, pulvinar leo eu, accumsan metus. Maecenas tempor malesuada dapibus. Etiam egestas purus nec turpis facilisis sollicitudin. Nulla rutrum volutpat metus, ut convallis mauris dictum id.',
+
+'Curabitur sed sapien condimentum, dapibus augue et, tristique nulla. Suspendisse a elementum tellus. Suspendisse viverra maximus metus, ut tristique odio laoreet ut. Sed at sem id tortor fermentum consectetur. Etiam et facilisis leo. Duis imperdiet lacus sed hendrerit interdum. Etiam interdum felis id dictum ullamcorper. Cras ultricies sapien vitae augue varius fringilla. Nullam a tellus laoreet tellus luctus tempor. Praesent ac erat at ex sagittis euismod at sed augue. Aenean vitae risus neque. In hac habitasse platea dictumst. Sed volutpat aliquet sagittis. In at tempor augue. Nunc id est mauris. Integer quis sagittis justo.',
+
+'Fusce non magna vitae dolor bibendum eleifend aliquet et dolor. In tristique nulla eu urna volutpat egestas. Aliquam ut tellus massa. Sed vestibulum risus diam, vitae rutrum est placerat eu. Morbi pulvinar eros elit, eu consectetur leo ultrices tempus. Vestibulum vitae facilisis nulla. Aliquam erat volutpat. Duis eget efficitur enim.',
+
+'Fusce malesuada suscipit tortor sit amet ultrices. Nullam sit amet orci eget libero sodales ultrices cursus a nisl. Suspendisse potenti. In efficitur massa a libero faucibus, at feugiat magna tincidunt. Nunc posuere orci non odio pharetra tincidunt. Morbi et aliquet elit, eu consectetur quam. Etiam dapibus ex et augue porttitor pulvinar sed et urna. Cras pulvinar leo nec mattis elementum. Pellentesque nec nibh quis lacus tincidunt feugiat.',
+
+'Nulla facilisi. Maecenas felis ex, luctus vel pellentesque vel, rhoncus vel enim. Proin euismod ultricies arcu, vitae sollicitudin odio mollis id. Vivamus vitae velit ac neque commodo dapibus nec vitae mauris. Nam pellentesque dictum eleifend. Suspendisse potenti. Nulla efficitur blandit mauris, rhoncus bibendum risus placerat et. Vestibulum vel ullamcorper mi. Nunc quis mauris vitae ante vestibulum dictum. Nam consequat elit tellus, in ultricies risus facilisis vitae. Pellentesque ac hendrerit velit. Curabitur non arcu suscipit, lobortis purus non, tincidunt justo. Proin porttitor hendrerit lectus, in egestas lectus. Praesent a fermentum mi.',
+
+'Duis sem orci, tempus ac malesuada in, auctor vel metus. Aenean id nibh tempus, egestas metus vitae, tristique sapien. Proin blandit est at tempus iaculis. Vestibulum maximus tortor sed ligula consectetur, eget venenatis enim cursus. Maecenas et odio massa. Vivamus at tincidunt quam, sed facilisis sapien. Etiam sed metus eget nisi rutrum accumsan. Nam imperdiet, orci ac viverra congue, magna nunc consectetur est, nec ultrices enim nulla eget leo. Aenean at ante metus. Duis posuere mauris ligula, finibus sodales augue placerat id.',
+
+'Nullam rhoncus elit vel finibus euismod. Vivamus ante orci, imperdiet et libero ut, tempor sagittis sem. Nullam metus mauris, sodales at dolor ut, scelerisque varius magna. Maecenas vitae mi tortor. Integer tempor tortor a ipsum pellentesque, vel malesuada libero elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec consectetur risus sed augue semper, eu rhoncus dui elementum. Aliquam posuere eu nibh ac ornare. Donec feugiat gravida suscipit. Nullam fringilla arcu id turpis sagittis luctus eget ut orci. Quisque vel purus accumsan, imperdiet ipsum eu, mattis risus. Etiam dapibus molestie enim convallis pellentesque. Donec ut nisi at orci ultrices ultricies vel eu tortor.',
+
+'Praesent elementum dolor nec ipsum consectetur, sit amet porta nisi aliquet. Ut commodo finibus erat sit amet facilisis. Sed mattis dui sit amet sapien convallis suscipit. Quisque elementum ipsum enim, non fermentum mi lacinia sed. Sed varius, purus ac facilisis imperdiet, dolor nisi dapibus libero, nec pulvinar leo arcu semper risus. Phasellus eu laoreet ipsum. Donec luctus faucibus ipsum convallis dignissim. Aliquam a tortor pretium, finibus nunc non, pellentesque arcu. Praesent malesuada porttitor nibh ut feugiat. Vestibulum in eleifend risus. Etiam et efficitur eros, at aliquet lacus. In consectetur dignissim turpis, facilisis maximus erat mollis id. Curabitur ultrices sagittis purus sed bibendum.',
+
+'Curabitur efficitur justo vel pharetra blandit. Nullam lacinia fermentum erat vel dictum. Sed maximus non neque posuere vulputate. Mauris ipsum ligula, mattis a elit in, placerat pretium mi. Sed at porta dui. Sed sollicitudin mauris non augue suscipit, id accumsan lacus vulputate. In sit amet diam in urna porttitor feugiat non lobortis enim. Etiam tincidunt dui mi, sed bibendum metus laoreet ut. Vivamus id ornare lectus.'];
+// oporer sob kota paragraph r por ',' dite hbe noyto problem hote pare
+
+
+
+const item = document.getElementById("items");
+const dataContainer = document.getElementById("data");
+
+function shuffle(array){
+
+    let currentIndex = array.length;
+    let randomIndex;
+
+
+    while(currentIndex != 0){
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+// stack overflow algorithm code for shuffle array elements
+        [array[currentIndex], array[randomIndex]] = [
+         array[randomIndex], array[currentIndex]   
+        ];
+    }
+    return array;
+}
+
+
+
+function generate(){
+
+    if(item.value == 0 || item.value < 0){
+        alert("The value can not be zero or less");
+    }
+    else if(item.value > paragraphs.length){
+       const randomIndex = Math.floor(Math.random() * paragraphs.length); 
+       
+       dataContainer.innerHTML = `${paragraphs[randomIndex]}`;
+    }
+    else{
+        const shuffelParagraph = paragraphs;
+        shuffle(paragraphs);
+
+
+        const selectedParagraphs = shuffelParagraph.slice(0, item.value);
+        const paragraphHTML = selectedParagraphs.map(paragraphs => `<p>${paragraphs}</p>`).join("");
+        dataContainer.innerHTML = paragraphHTML;
+    }
+}
